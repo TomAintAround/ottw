38bf6049a66c53c0c76179e3f9181c2abe74a868
TomAintAround <tcorreia2005@proton.me>
2025-08-07 03:02:41 +0100

    chore: initial commit

---
+++ flake.nix
@@ -0,0 +1,63 @@
+{
+  description = "Flake for my widgets";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    astal = {
+      url = "github:aylur/astal";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    astal,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        nativeBuildInputs = with pkgs; [
+          gobject-introspection
+          pkg-config
+        ];
+
+        buildInputs = [
+          pkgs.glib
+          pkgs.gtk4
+          astal.packages.${system}.io
+          astal.packages.${system}.astal4
+        ];
+      in {
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "tommWidgets";
+          version = "0.1";
+          src = ./.;
+
+          inherit nativeBuildInputs buildInputs;
+
+          buildPhase = ''
+            mkdir -p build
+            make
+          '';
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp build/widget $out/bin
+          '';
+        };
+
+        devShells.default = pkgs.mkShell {
+          inherit buildInputs nativeBuildInputs;
+
+          shellHook = ''
+            export CFLAGS="$(pkg-config --cflags glib-2.0 gtk4 astal-4-4.0)"
+            export LDFLAGS="$(pkg-config --libs glib-2.0 gtk4 astal-4-4.0)"
+          '';
+        };
+      }
+    );
+}
