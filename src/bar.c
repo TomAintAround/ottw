17ac0a8c1f70812385a2119afdda492e7010cb07
TomAintAround <tcorreia2005@proton.me>
2025-08-17 16:37:15 +0100

    feat(bar): add time and date labels

--- src/bar.c
+++ src/bar.c
@@ -2,6 +2,30 @@
 #include <gtk4-layer-shell/gtk4-layer-shell.h>
 #include "bar.h"
 
+static bool getTime(GObject* timeLabel) {
+	const time_t now = time(0);
+	const struct tm* timeInfo = localtime(&now);
+	const int hour = timeInfo->tm_hour;
+	const int minutes = timeInfo->tm_min;
+	char timeString[8];
+	sprintf(timeString, "%d:%d", hour, minutes);
+	gtk_label_set_label(GTK_LABEL(timeLabel), timeString);
+
+	return true;
+}
+
+static bool getDate(GObject* dateLabel) {
+	const time_t now = time(0);
+	const struct tm* timeInfo = localtime(&now);
+	const int day = timeInfo->tm_mday;
+	const int month = timeInfo->tm_mon;
+	char dateString[8];
+	sprintf(dateString, "%d/%d", day, month);
+	gtk_label_set_label(GTK_LABEL(dateLabel), dateString);
+
+	return true;
+}
+
 static void realize(GtkWindow* window, GdkMonitor* monitor) {
 	const gboolean anchors[] = { FALSE, TRUE, TRUE, TRUE };
 	gtk_layer_init_for_window(window);
@@ -27,6 +51,14 @@ void bar(GtkApplication* app, GdkMonitor* monitor) {
 	gtk_style_context_add_provider_for_display(gtk_widget_get_display(GTK_WIDGET(window)),
 											   GTK_STYLE_PROVIDER(cssProvider),
 											   GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+
+	GObject* timeLabel = gtk_builder_get_object(ui, "time");
+	GObject* dateLabel = gtk_builder_get_object(ui, "date");
+	// Get time first, then start loop
+	getTime(timeLabel);
+	getDate(dateLabel);
+	g_timeout_add_seconds(1, G_SOURCE_FUNC(getTime), timeLabel);
+	g_timeout_add_seconds(1, G_SOURCE_FUNC(getDate), dateLabel);
 
 	gtk_widget_set_visible(GTK_WIDGET(window), TRUE);
 	g_object_unref(ui);
